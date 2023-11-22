ae18e794e40748cb199ab166dec52aa00f9bf6b6
AlwanKarsa <karsagamers@gmail.com>
2023-11-22 16:24:32 +0800

    settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
-global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global.namabot = "Kurumi Tokisaki"
+global.namaowner = "AlwnKrsa"
+global.packname = Kurumi
+global.author = AlwanKrsa
+global.myweb = "https://s.id/AlwnKrsa"
+global.footer_text = "© " + Kurumi
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['085656904288']
 global.pairingNumber = "" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
